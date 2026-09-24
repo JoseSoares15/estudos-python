929a3d65983441fd856f87a1f8ab0af605391907
JoseSoares15 <josefsoares45@gmail.com>
2026-09-24 12:08:20 -0300

    interatividade

---
+++ aulas/03-interatividade.py
@@ -0,0 +1,6 @@
+# Agora vamos deixar o exemplo usado na aula anterior mais interativo, vamos pedir para o usuário digitar o nome, idade e peso dele e depois exibir na tela.
+
+nome = input('Qual é o seu nome? ')
+idade = input('Quantos anos você tem? ')
+peso = input('Quanto você pesa? ')
+print(nome, idade, peso)
