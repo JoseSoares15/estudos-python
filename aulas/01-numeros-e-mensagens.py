ed1719fec152b39fddae6f0d639b4daae8e745e3
JoseSoares15 <josefsoares45@gmail.com>
2026-09-24 11:47:11 -0300

    Primeiros exercicios em Python

---
+++ aulas/01-numeros-e-mensagens.py
@@ -0,0 +1,15 @@
+# Os dados em python, se forem mensagens tem delimitadores especiais.
+
+print('Olá, mundo!')
+print(7+4)
+print('7'+'4')
+
+# Para o python3 todos os comandos são funções logo toda função tem que ter parenteses
+
+# Mensagens ≠ números
+
+# Mensagens são primordialmente usadas para serem exibidas na tela. Números são primordialmente usados para operações matemáticas.
+
+# Em números não são usadas aspas, mas se usadas, o python ira juntar os dois números pois o simbolo mais ou virgula junta os elementos, mas não faz operações matemáticas.
+
+#Toda mensagem TEM que estar entre aspas ou dara erro de sintaxe
