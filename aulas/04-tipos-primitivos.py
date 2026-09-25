29ee7b32349d985c1b476897a3dde3481ee3909f
JoseSoares15 <josefsoares45@gmail.com>
2026-09-25 10:39:25 -0300

    corrigido formato da string na impressão da soma

--- aulas/04-tipos-primitivos.py
+++ aulas/04-tipos-primitivos.py
@@ -21,4 +21,4 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro: '))
 s = n1 + n2
-print('A soma entre {} e {} é {}' .format(n1, n2, s))
+print('A soma entre {0} e {1} é {2}' .format(n1, n2, s))
