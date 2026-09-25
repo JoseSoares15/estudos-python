2493bfc430e95557aeb2ff2b0980a7325c57175b
JoseSoares15 <josefsoares45@gmail.com>
2026-09-25 10:45:22 -0300

    ex003

---
+++ exercicios/ex003.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia dois números e mostre a soma entre eles
+n1 = int(input('Digite um númmero: '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+print('A soma entre {0} e {1} é {2}' .format(n1, n2, s))
