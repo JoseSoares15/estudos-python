75d5508907009d07b00934873d44aab86c810ac2
JoseSoares15 <josefsoares45@gmail.com>
2026-09-26 11:46:44 -0300

    treino de is

---
+++ exercicios/ex004b.py
@@ -0,0 +1,6 @@
+# alguas práticas novas pois não entendi bem o is
+p = input('Digite algo: ')
+print('É numérico? ', p.isnumeric())
+print('É alfanumerico? ', p.isalnum())
+print('É decimal? ', p.isdecimal())
+print('Só tem letras? ', p.isalpha())
